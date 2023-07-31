b66c980703fcc586d264bbe92ee26322e7ae9da8
Саша Калинин <kalininsasa38@gmail.com>
2023-07-31 14:52:37 +0300

    Update urls.py

--- shope/shope/urls.py
+++ shope/shope/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include("auth_app.urls"))
+    path('auth_app/', include("auth_app.urls"))
 ]
 
 
