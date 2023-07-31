5632fd4720c4f7805df5e60b997b13c58b86c128
Мейн Иван Сергеевич <Buh02@MV-SERVICE.MVSO>
2023-07-31 16:20:20 +0300

    create app prifile_app, update index.html in core s app , added normalize.css

---
+++ shope/profile_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views import View
+
+
+class AccountView(View):
+    pass
+
+
+class ProfileView(View):
+    pass
+
+
+class ProfileAvatarView(View):
+    pass
