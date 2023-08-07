19ee2d8f01b6a9eb2af9936f57c320aa8b57215e
sasha_kalinin <kalininsasa38@gmail.com>
2023-08-07 12:07:50 +0300

    fixbugs/change_auth_urls changed url structure

--- shope/auth_app/urls.py
+++ shope/auth_app/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import WebPasswordResetView, EmailView, RegisterView
 
 urlpatterns = [
-    path('restore_password/', WebPasswordResetView.as_view(), name='restore_password'),
+    path('restore_password/password', WebPasswordResetView.as_view(), name='restore_password'),
     path('login/', LoginView.as_view(template_name="auth/login.html"), name="login"),
-    path("e-mail/", EmailView.as_view(), name="e-mail"),
+    path("restore_password/e-mail/", EmailView.as_view(), name="e-mail"),
     path("register/", RegisterView.as_view(), name="register"),
 ]
