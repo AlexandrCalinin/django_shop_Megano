38455a2a3117c1f3043e1023b1b80494bdf6ac23
sasha_kalinin <kalininsasa38@gmail.com>
2023-08-10 22:20:36 +0300

    feature/add_user_model added user model

--- shope/auth_app/models.py
+++ shope/auth_app/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    username = models.CharField(unique=False, default="", max_length=48, null=False, blank=False)
+    email = models.EmailField(unique=True, default="", null=False, blank=False)
+    middle_name = models.CharField(default="", null=False, blank=False, max_length=48)
+    activation_key = models.CharField(default="", null=False, blank=False, max_length=48)
+    activation_key_expires = models.CharField(default="", null=False, blank=False, max_length=48)
+    is_activation_key_expires = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
