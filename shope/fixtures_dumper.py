1c0a52e149a5d404e5cab0578c8dce5ac48022a5
Ivan <main_ivan@mail.ru>
2023-09-03 02:32:24 +0300

    fixtures

--- shope/fixtures_dumper.py
+++ shope/fixtures_dumper.py
@@ -3,5 +3,6 @@ import os
 
 data1 = os.system("python -Xutf8 manage.py dumpdata catalog_app.Category > fixtures/category.json")
 data2 = os.system("python -Xutf8 manage.py dumpdata catalog_app.Product > fixtures/product.json")
-data4 = os.system("python -Xutf8 manage.py dumpdata taggit > fixtures/tags.json")
-data3 = os.system("python -Xutf8 manage.py dumpdata catalog_app.Image > fixtures/image.json")
+data3 = os.system("python -Xutf8 manage.py dumpdata taggit.Tag > fixtures/tag.json")
+data4 = os.system("python -Xutf8 manage.py dumpdata taggit.TaggedItem > fixtures/taggeditem.json")
+data5 = os.system("python -Xutf8 manage.py dumpdata catalog_app.Image > fixtures/image.json")
--- shope/fixtures_loader.py
+++ shope/fixtures_loader.py
@@ -3,5 +3,6 @@ import os
 
 data1 = os.system("python manage.py loaddata category.json")
 data2 = os.system("python manage.py loaddata product.json")
-data4 = os.system("python manage.py loaddata tags.json")
-data3 = os.system("python manage.py loaddata image.json")
+data3 = os.system("python manage.py loaddata tag.json")
+data4 = os.system("python manage.py loaddata taggeditem.json")
+data5 = os.system("python manage.py loaddata image.json")
