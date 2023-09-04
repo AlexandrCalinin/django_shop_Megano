68bf3f08e94b512b777ec1fd4e9152a42c46e526
sasha_kalinin <kalininsasa38@gmail.com>
2023-09-04 22:48:43 +0300

    feature/login_and_registration_fixed done!

--- shope/auth_app/urls.py
+++ shope/auth_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from .views import SetNewPasswordView, ForgotPasswordView, \
-    RegisterView, UserLogoutView, VerifyEmailView, UserLoginView
+    RegisterView, UserLogoutView, UserLoginView
 
 app_name = 'auth_app'
 
@@ -11,7 +11,7 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('forgot-password/', ForgotPasswordView.as_view(), name='e-mail'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('confirm-email/', TemplateView.as_view(template_name='auth/confirm-email.html'), name='confirm-email'),
-    path('verify_email/<email>/<activate_key>/', VerifyEmailView.as_view(), name='verify_email'),
+    path('confirm-email/', TemplateView.as_view(template_name='auth_app/confirm-email.html'), name='confirm-email'),
+    path('verify_email/<email>/<activate_key>/', RegisterView.verify, name='verify_email'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
