c01e499ff934022611a923bfef64744367823ad1
MAX <vsadnik9199@mail.ru>
2023-09-09 10:55:23 +0300

    fixtures users, sellers, prices

--- shope/fixtures_dumper.py
+++ shope/fixtures_dumper.py
@@ -6,3 +6,6 @@ os.system("python -Xutf8 manage.py dumpdata catalog_app.Image > fixtures/3_image
 os.system("python -Xutf8 manage.py dumpdata taggit.Tag > fixtures/5_tag.json")
 os.system("python -Xutf8 manage.py dumpdata taggit.TaggedItem > fixtures/7_taggeditem.json")
 os.system("python -Xutf8 manage.py dumpdata catalog_app.Product > fixtures/9_product.json")
+os.system("python -Xutf8 manage.py dumpdata auth_app.User > fixtures/10_user.json")
+os.system("python -Xutf8 manage.py dumpdata core.Seller > fixtures/11_seller.json")
+os.system("python -Xutf8 manage.py dumpdata core.Price > fixtures/12_price.json")
