98377a7fd6e01400989f282eddfcc38967cf6543
sasha_kalinin <kalininsasa38@gmail.com>
2023-09-19 15:12:13 +0300

    feature/check_user_auth deleted user form view and added logout

--- shope/core/views.py
+++ shope/core/views.py
@@ -13,7 +13,6 @@ class BaseView(TemplateView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['user_is_authenticated'] = self.request.user.is_authenticated
         context['product_top_list'] = self._product_top_list.get_product_top_list(const=8)
 
         return context
