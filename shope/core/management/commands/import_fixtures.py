0f02359f17175396f08564134ce06c4b2ef3a864
MAX <vsadnik9199@mail.ru>
2023-10-09 13:49:05 +0300

    command for import fixtures

--- shope/core/management/commands/import_fixtures.py
+++ shope/core/management/commands/import_fixtures.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from django.core.management.base import BaseCommand
 import os
 import logging
@@ -5,13 +6,19 @@ import shutil
 from django.core.mail import send_mail
 from django.conf import settings
 
-def log(level, filename):
-    logging.basicConfig(level=level,
-                        filename=filename,
-                        filemode='w',
-                        format='%(asctime)s -%(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 
-    logger = logging.getLogger('log')
+
+def setup_logger(name, log_file, level=logging.INFO):
+    """Нстройка логов"""
+
+    handler = logging.FileHandler(log_file, mode='w')
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
     return logger
 
 
@@ -19,35 +26,38 @@ class Command(BaseCommand):
     help = 'import fixtures'
 
     def add_arguments(self, parser):
-        parser.add_argument('-p', '--filename', type=str)
-        parser.add_argument('-d', '--email', type=str)
+        parser.add_argument('-f', '--filename', type=str)
+        parser.add_argument('-e', '--email', type=str)
 
     def handle(self, *args, **options):
-        logger = log(logging.INFO, 'import/successful_import/success_log.log')
-        error_logger = log(logging.ERROR, 'import/unsuccessful_import/unsuccess_log.log')
+        logger = setup_logger('log', 'import/successful_import/success_log.log', logging.INFO)
+        error_logger = setup_logger('error', 'import/unsuccessful_import/unsuccess_log.log', logging.ERROR)
 
         if options['filename']:
             fixture = options['filename']
-            try:
-                os.system("python manage.py loaddata %s" % fixture)
+            s = os.system("python manage.py loaddata %s" % fixture)
+            if not s:
                 logger.info(f'{fixture} was uploaded')
                 shutil.copy2(f'fixtures/{fixture}', 'import/successful_import')
-            except:
+            else:
+                error_logger.error(f'an error occurred in the {fixture} file')
+                shutil.copy2(f'fixtures/{fixture}', 'import/unsuccessful_import')
 
-                error_logger.error((f'{fixture} not upload'))
         else:
             for fixture in sorted(os.listdir('fixtures'), reverse=False):
-                try:
-                    os.system("python manage.py loaddata %s" % fixture)
+                s = os.system("python manage.py loaddata %s" % fixture)
+                if not s:
                     shutil.copy2(f'fixtures/{fixture}', 'import/successful_import')
                     logger.info(f'{fixture} was uploaded')
-                except:
-                    error_logger.error(f'in {fixture} was error: ')
+                else:
+                    error_logger.error(f'an error occurred in the {fixture} file')
                     shutil.copy2(f'fixtures/{fixture}', 'import/unsuccessful_import')
 
         if options['email']:
+            with open('import/unsuccessful_import/unsuccess_log.log', 'r') as file:
+                head = ''.join(file.readlines(1))
             send_mail('Зарузка фикстур',
-                      'fixtures uploaded',
+                      head,
                       settings.EMAIL_HOST_USER,
                       [options['email']])
 
