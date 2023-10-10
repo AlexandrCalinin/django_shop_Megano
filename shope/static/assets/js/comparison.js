a4ba139dffe3931856b5a767680f14fc85c5125c
Buh02 <main_ivan@mail.ru>
2023-10-10 16:47:18 +0300

    comparison.js as well

--- shope/static/assets/js/comparison.js
+++ shope/static/assets/js/comparison.js
@@ -3,47 +3,52 @@
     var form = $('.comparison-add');
     console.log('принта', form);
 
-    function compareUpdating(product_id) {
-        var data = {};
-        data.product_id = product_id;
-        var csrf_token = $('#comparison-add [name="csrfmiddlewaretoken"]').val();
-        data["csrfmiddlewaretoken"] = csrf_token;
-        var url = form.attr("action");
-        console.log('принт дата', data);
-        $.ajax({
-            url: url,
-            type: 'POST',
-            data: data,
-            cache: true,
-            success: function (data) {
-                console.log('OK');
-                console.log(data.product_id);
-            },
-            error: function () {
-                console.log("error")
-            }
-        })
+    function compareUpdating(product_id, url) {
+      var data = {};
+      data.product_id = product_id;
+      var csrf_token = $('meta[name="csrf-token"]').attr('content');
+      data["csrfmiddlewaretoken"] = csrf_token;
+      console.log('принт url', url);
+      console.log('принт дата', data);
+      $.ajax({
+        url: url,
+        type: 'POST',
+        data: data,
+        cache: true,
+        success: function (data) {
+          console.log('OK');
+          console.log(data.product_id);
+        },
+        error: function () {
+          console.log("error")
+        }
+      })
     }
 
-//    form.on('submit', function(e) {
-//      e.preventDefault();
-//      console.log('123');
-//      var compare_btns = $('.compare_btn');
-//      console.log("compare_btns", compare_btns)
-//      $.each(compare_btns, function(k, v) {
-//          console.log('v', v);
-//          var product_id = v.data('product_id');
-//          console.log('v', v.data());
-//          compareUpdating(prod_id)
-//      });
-//    });
-    $(".compare_btn").on("click", "button[class='compare_btn']", function() {
-
-      var product_id = $(this)
-
-      console.log('product_id', product_id);
-      compareUpdating(prod_id)
-    })
-
+    form.on('submit', function(e) {
+      e.preventDefault();
+      console.log('123');
+      var compare_btns = $('.compare_btn');
+      console.log("compare_btns", compare_btns)
+      $.each(compare_btns, function(k, v) {
+        console.log('v', v);
+        console.log('k', k);
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          var product_id = $(this).attr('data-product_id')
+          var url = $(this).parents('.comparison-add').attr('action')
+          console.log('product_id', product_id);
+          compareUpdating(product_id, url)
+        })
+        console.log($(this).attr('data-product_id'));
+//          v.addEventListener('click', onBtnClick);
+//          v.addEventListener('click', function(event) {
+//            event.preventDefault();
+//            console.log('clicked');
+//          });
+//          compareUpdating(product_id)
+      });
+    });
   });
 })();
+
