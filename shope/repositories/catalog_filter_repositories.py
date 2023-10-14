6dfe2308677961dace51efa8d28c30a335995932
sasha_kalinin <kalininsasa38@gmail.com>
2023-10-15 00:20:20 +0300

    feature/catalog_add_sort_method added normal sort

--- shope/repositories/catalog_filter_repositories.py
+++ shope/repositories/catalog_filter_repositories.py
@@ -60,16 +60,17 @@ class CatalogFilterRepository:
                     return self.queryset.filter(min_price__range=(product_min_price, product_max_price))
 
     @beartype
-    def filter_by_tag(self, tag_name: Any) -> QuerySet[Product]:
+    def filter_by_tag(self, tag_name: str) -> QuerySet[Product]:
         tags = Tag.objects.filter(slug=tag_name).values_list('name', flat=True)
         return self.queryset.filter(tag__name__in=tags)
 
     @beartype
-    def filter_by_sort(self, sort: Any) -> QuerySet[Product]:
+    def filter_by_sort(self, sort: str, query: QuerySet[Product]) -> QuerySet[Product]:
         if sort == "min_price":
-            return self.queryset.order_by(sort)
+            print(query)
+            return query.order_by(sort)
         else:
-            return self.queryset.order_by(f"-{sort}")
+            return query.order_by(f"-{sort}")
 
     @beartype
     def get_filtered_products_by_category(self, _category_id: str) -> QuerySet[Product]:
