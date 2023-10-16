7f8a873c2ec24ed28357be97373c9db0c012a2d6
Ivan <main_ivan@mail.ru>
2023-10-17 00:51:14 +0300

    somefix

--- shope/repositories/compare_product_repositories.py
+++ shope/repositories/compare_product_repositories.py
@@ -1,8 +1,15 @@
 from beartype import beartype
-from django.db.models import QuerySet
+from django.db.models import QuerySet, OuterRef, Min, F, Count, Max, DateField, ExpressionWrapper, DurationField, \
+    fields, DateTimeField, IntegerField, FloatField, Avg, When, Case, Q
+from django.db.models.functions import Cast, Extract
 
 from catalog_app.models import CompareProduct, CharacteristicType
+from core.models import Price
 from interface.compare_product_interface import ICompareProduct
+
+
+from django.utils import timezone
+today = timezone.now().date()
 
 
 class CompareProductRepository(ICompareProduct):
@@ -10,10 +17,40 @@ class CompareProductRepository(ICompareProduct):
     @beartype
     def get_compare_product_list(self, _session_key: str) -> QuerySet[CompareProduct]:
         """Вернуть кверисет продуктов для сравнения"""
-        return CompareProduct.objects.filter(session_key=_session_key).prefetch_related(
+        qs = CompareProduct.objects.filter(session_key=_session_key).prefetch_related(
             'product__category__characteristictype_set',
             'product__characteristics'
         )
+        # lst = [i.product.id for i in qs]
+        print([i.product.id for i in qs])
+        today = Cast(timezone.now().date(), output_field=DateTimeField())
+        date = F('product__price__created_at')
+        duration_in_microseconds = Cast(today - date, output_field=IntegerField())
+        new = qs.annotate(product_price=Min(F('product__price__price')), duration=Min(duration_in_microseconds))
+        # product_seller = F('product__price__seller__name'),
+
+        # last_price = Price.objects.filter(product__in=lst).annotate(duration=Min(duration_in_microseconds))
+        # print(last_price, last_price.created_at)
+        for i in new:
+            print(i, i.product_price, i.duration)
+
+        # today = Cast(timezone.now().date(), output_field=DateTimeField())
+        # date = F('created_at')
+        # duration_in_microseconds = Cast(today - date, output_field=IntegerField())
+        min_price_subquery = Price.objects.filter(product=3).annotate(duration=Min(duration_in_microseconds))
+        for i in min_price_subquery:
+            print(i, i.created_at, i.duration)
+        #
+        # last_price = Price.objects.all().prefetch_related('product').latest('created_at')
+        # print(last_price.product.pk, last_price, last_price.date)
+
+        # print(min_price_subquery)
+        # sell = CompareProduct.objects.filter(session_key=_session_key).annotate(seller_product=F('product__price__seller__name'))
+        # print(sell)
+        # for i in sell:
+        #     print(i, i.seller_product)
+
+        return qs
 
     @beartype
     def create_compare_product(self, _product_id: int, _session_key: str) -> None:
